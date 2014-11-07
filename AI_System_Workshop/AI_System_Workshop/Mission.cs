aa7dce732df34e0e89824690f3009d881b67c448
Rohun Banerji <rohunb@gmail.com>
2014-11-06 22:41:23 -0500

    added Mission class

---
+++ AI_System_Workshop/AI_System_Workshop/Mission.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_System_Workshop
+{
+    class Mission
+    {
+        public enum MissionType { EliminateEnemies, DestroyEnemyStructure, Escort, DistressCall };
+        MissionType missionType;
+    }
+}
