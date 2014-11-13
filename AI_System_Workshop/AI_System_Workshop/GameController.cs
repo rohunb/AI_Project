2a0b69c86064e4307d08191051184f06a9d5afaf
rohunb <rohunb@gmail.com>
2014-11-13 16:05:41 -0500

    added gameController class which control the scene changes

---
+++ AI_System_Workshop/AI_System_Workshop/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_System_Workshop
+{
+    struct PreSceneChangeArgs
+    {
+
+    }
+    struct PostSceneChangeArgs
+    {
+
+    }
+    class GameController
+    {
+        public delegate void PreSceneChange(PreSceneChangeArgs args);
+        public event PreSceneChange OnPreSceneChange = new PreSceneChange((PreSceneChangeArgs) => { });
+
+        public delegate void PostSceneChange(PostSceneChangeArgs args);
+        public event PostSceneChange OnPostSceneChange = new PostSceneChange((PostSceneChangeArgs) => { });
+
+        GameController()
+        {
+ 
+        }
+
+    }
+}
